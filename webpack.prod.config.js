468a83b974fc6e4c4f96a574913549b52e434d03
Martin <martinkkwan@gmail.com>
2017-02-01 12:50:54 -0800

    (refactor) Edit .gitignore

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -29,12 +29,6 @@ module.exports = {
     extensions: ['', '.js', '.jsx'],
   },
   plugins: [
-    new webpack.optimize.UglifyJsPlugin({
-      minimize: true,
-      compress: {
-        warnings: false,
-      },
-    }),
     new webpack.DefinePlugin({
       'process.env': {
         NODE_ENV: JSON.stringify('production'),
