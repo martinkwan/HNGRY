5fbc28c87ccd6f64ec4e3bdeb458a8717124be23
Martin <martinkkwan@gmail.com>
2017-02-01 12:27:36 -0800

    (feat) Add uglify to prod webpack

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -29,6 +29,12 @@ module.exports = {
     extensions: ['', '.js', '.jsx'],
   },
   plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      minimize: true,
+      compress: {
+        warnings: false,
+      },
+    }),
     new webpack.DefinePlugin({
       'process.env': {
         NODE_ENV: JSON.stringify('production'),
